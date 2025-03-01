98b82d977817bd05ed41b32d1053e8b7401c2ceb
Deepu1004 <varanasisaideepak@gmail.com>
2025-03-01 19:40:55 +0530

    App jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-import React from 'react';
-import Header from './components/Header'; 
-import Hero from './components/Hero'; 
-import About from './components/About'; 
-import Projects from './components/Projects'; 
-import Skills from './components/Skills'; 
-import Contact from './components/Contact'; 
-import Footer from './components/Footer'; 
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 
 function App() {
